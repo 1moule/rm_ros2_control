4da020ce129162d2acb7cbab12423a0cbfd8d0f1
1moule <1961466188@qq.com>
2025-02-11 12:14:54 +0800

    Overload td update.

--- rm_ros2_common/include/rm_ros2_common/decision/trajectory_planner.hpp
+++ rm_ros2_common/include/rm_ros2_common/decision/trajectory_planner.hpp
@@ -18,37 +18,42 @@ public:
     }
     void clear(T x1, T x2)
     {
-        x1_ = x1;
-        x2_ = x2;
+      x1_ = x1;
+      x2_ = x2;
     }
     void update(T v, T v_dot)
     {
-        if ((v > 3. && x1_ < -3.) || (v < -3 && x1_ > 3))
-        {
-            x1_ = v;
-            x2_ = 0;
-        }
-        else
-        {
-            T y = x1_ - v + h_ * x2_;
-            T a0 = sqrt(h_ * h_ * r_ * r_ + 4 * r_ * fabs(y));
-            T a = x2_ + 0.5 * (a0 - h_ * r_) * (y > 0 ? 1 : -1);
-            T u = fabs(a) > h_ * r_ ? -r_ * (a > 0 ? 1 : -1) : -r_ * a / (h_ * r_);
-            x1_ = x1_ + h_ * (x2_ + v_dot);
-            x2_ = x2_ + h_ * u;
-        }
+      if ((v > 3. && x1_ < -3.) || (v < -3 && x1_ > 3))
+      {
+        x1_ = v;
+        x2_ = 0;
+      }
+      else
+      {
+        T y = x1_ - v + h_ * x2_;
+        T a0 = sqrt(h_ * h_ * r_ * r_ + 8 * r_ * fabs(y));
+        T a = x2_ + 0.5 * (a0 - h_ * r_) * (y > 0 ? 1 : -1);
+        T u = fabs(a) > h_ * r_ ? -r_ * (a > 0 ? 1 : -1) : -r_ * a / (h_ * r_);
+        x1_ = x1_ + h_ * (x2_ + v_dot);
+        x2_ = x2_ + h_ * u;
+      }
     }
+    void update(T v)
+    {
+      update(v, 0.);
+    }
+
     T getX1() const
     {
-        return x1_;
+      return x1_;
     }
     T getX2() const
     {
-        return x2_;
+      return x2_;
     }
 
 private:
-    T r_{}, h_{};
-    T x1_{}, x2_{};
+  T r_{}, h_{};
+  T x1_{}, x2_{};
 };
 #endif //TRAJECTORY_PLANNER_HPP
