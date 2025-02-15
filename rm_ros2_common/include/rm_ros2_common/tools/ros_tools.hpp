fd27b8f1f9ed991b192cfc35e55725be577b0ce9
1moule <1961466188@qq.com>
2025-02-15 16:24:04 +0800

    Modify getParam function.

--- rm_ros2_common/include/rm_ros2_common/tools/ros_tools.hpp
+++ rm_ros2_common/include/rm_ros2_common/tools/ros_tools.hpp
@@ -7,14 +7,13 @@
 #include <rclcpp/rclcpp.hpp>
 
 template <typename ParameterT>
-auto getParam(rclcpp::Node::SharedPtr& node, const std::string& name, const ParameterT& default_value)
+bool getParam(rclcpp::Node::SharedPtr& node, const std::string& name, ParameterT& value, const ParameterT& default_value)
 {
   if (!node->has_parameter(name))
   {
-    return node->declare_parameter<ParameterT>(name, default_value);
+    value = node->declare_parameter<ParameterT>(name, default_value);
+    return false;
   }
-  else
-  {
-    return node->get_parameter(name).get_value<ParameterT>();
-  }
+  value = node->get_parameter(name).get_value<ParameterT>();
+  return true;
 }
