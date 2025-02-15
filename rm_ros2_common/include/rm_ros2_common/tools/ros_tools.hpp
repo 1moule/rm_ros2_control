5a541cbff3e15260565fb369eb125b1171f40aa6
1moule <1961466188@qq.com>
2025-02-15 21:19:54 +0800

    Overload getParam.

--- rm_ros2_common/include/rm_ros2_common/tools/ros_tools.hpp
+++ rm_ros2_common/include/rm_ros2_common/tools/ros_tools.hpp
@@ -12,3 +12,20 @@ auto getParam(rclcpp::Node::SharedPtr& node, const std::string& name, const Para
   node->declare_parameter<ParameterT>(name, default_value);
   return node->get_parameter(name).get_value<ParameterT>();
 }
+
+template <typename ParameterT>
+bool getParam(const rclcpp::Node::SharedPtr& node, const std::string& name, const ParameterT& default_value,
+              ParameterT& value)
+{
+  rclcpp::Parameter param;
+  if (node->get_parameter(name, param))
+  {
+    value = param.get_value<ParameterT>();
+    return true;
+  }
+  else
+  {
+    value = default_value;
+    return false;
+  }
+}
